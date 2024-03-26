608fb0bcbcacdba21f8b625961a4377a4291b5a1
Youwen Wu <youwenw@gmail.com>
2024-03-25 17:13:19 -0700

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use gilrs::{Button, Event, EventType, Gilrs};
+use gilrs::{Button, Event, Gilrs};
 
 fn main() {
     let mut gilrs = Gilrs::new().unwrap();
@@ -10,31 +10,60 @@ fn main() {
 
     loop {
         // Examine new events
-        while let Some(event) = gilrs.next_event() {
-            match event.event {
-                EventType::Connected => {
-                    println!("Gamepad connected: {}", event.id);
+        while let Some(Event { id, event, time }) = gilrs.next_event() {
+            println!("{:?} New event from {}: {:?}", time, id, event);
+            match event {
+                gilrs::EventType::ButtonPressed(Button::South, _) => {
+                    println!("Button South pressed");
                 }
-
-                EventType::Disconnected => {
-                    println!("Gamepad disconnected: {}", event.id);
+                gilrs::EventType::ButtonPressed(Button::East, _) => {
+                    println!("Button East pressed");
                 }
-
-                EventType::ButtonPressed(button, _code) => {
-                    println!("Gamepad: {} Button Pressed: {:?}", event.id, button);
+                gilrs::EventType::ButtonPressed(Button::North, _) => {
+                    println!("Button North pressed");
                 }
-
-                EventType::ButtonReleased(button, _code) => {
-                    println!("Gamepad: {} Button Released: {:?}", event.id, button);
+                gilrs::EventType::ButtonPressed(Button::West, _) => {
+                    println!("Button West pressed");
                 }
-
-                EventType::AxisChanged(axis, value, _code) => {
-                    println!(
-                        "Gamepad: {} Axis Changed: {:?} Value: {}",
-                        event.id, axis, value
-                    );
+                gilrs::EventType::ButtonPressed(Button::LeftTrigger, _) => {
+                    println!("Left Trigger pressed");
                 }
-
+                gilrs::EventType::ButtonPressed(Button::LeftTrigger2, _) => {
+                    println!("Left Trigger 2 pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::RightTrigger, _) => {
+                    println!("Right Trigger pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::RightTrigger2, _) => {
+                    println!("Right Trigger 2 pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::Select, _) => {
+                    println!("Select button pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::Start, _) => {
+                    println!("Start button pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::Mode, _) => {
+                    println!("Mode button pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::LeftThumb, _) => {
+                    println!("Left Thumb pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::RightThumb, _) => {
+                    println!("Right Thumb pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::DPadUp, _) => {
+                    println!("D-Pad Up pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::DPadDown, _) => {
+                    println!("D-Pad Down pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::DPadLeft, _) => {
+                    println!("D-Pad Left pressed");
+                }
+                gilrs::EventType::ButtonPressed(Button::DPadRight, _) => {
+                    println!("D-Pad Right pressed");
+                }
                 _ => {}
             }
         }
