c4a6df73e5849b703c7d11d9d03ce62ffd7e7b8f
Youwen Wu <youwenw@gmail.com>
2024-03-25 17:48:42 -0700

    feat: detect keypresses

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,130 @@
-use gilrs::{Button, Event, Gilrs};
+use device_query::{DeviceQuery, DeviceState, Keycode};
+use std::collections::HashMap;
+use std::sync::mpsc::{channel, Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
-fn main() {
-    let mut gilrs = Gilrs::new().unwrap();
+type ActionFn = Box<dyn Fn() + Send + 'static>;
 
-    // Iterate over all connected gamepads
-    for (_id, gamepad) in gilrs.gamepads() {
-        println!("Connected gamepad: {:?}", gamepad.name());
+struct KeybindManager {
+    bindings: HashMap<Keycode, ActionFn>,
+}
+
+impl KeybindManager {
+    fn new() -> Self {
+        KeybindManager {
+            bindings: HashMap::new(),
+        }
     }
 
-    loop {
-        // Examine new events
-        while let Some(Event { id, event, time }) = gilrs.next_event() {
-            println!("{:?} New event from {}: {:?}", time, id, event);
-            match event {
-                gilrs::EventType::ButtonPressed(Button::South, _) => {
-                    println!("Button South pressed");
+    fn bind_key<F>(&mut self, key: Keycode, action: F)
+    where
+        F: Fn() + Send + 'static,
+    {
+        self.bindings.insert(key, Box::new(action));
+    }
+
+    fn handle_key_event(&self, key: Keycode) {
+        if let Some(action) = self.bindings.get(&key) {
+            action();
+        }
+    }
+}
+
+fn main() {
+    let (tx, rx): (Sender<Keycode>, Receiver<Keycode>) = channel();
+
+    let v_x = Arc::new(Mutex::new(0));
+    let v_y = Arc::new(Mutex::new(0));
+
+    // Spawn a separate thread to listen for key events
+    thread::spawn(move || {
+        let device_state = DeviceState::new();
+        loop {
+            let keys: Vec<Keycode> = device_state.get_keys();
+            for key in keys {
+                match key {
+                    Keycode::W | Keycode::Up => tx.send(Keycode::W).unwrap(),
+                    Keycode::A | Keycode::Left => tx.send(Keycode::A).unwrap(),
+                    Keycode::S | Keycode::Down => tx.send(Keycode::S).unwrap(),
+                    Keycode::D | Keycode::Right => tx.send(Keycode::D).unwrap(),
+                    Keycode::B | Keycode::Space => tx.send(Keycode::B).unwrap(),
+                    _ => (),
                 }
-                gilrs::EventType::ButtonPressed(Button::East, _) => {
-                    println!("Button East pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::North, _) => {
-                    println!("Button North pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::West, _) => {
-                    println!("Button West pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::LeftTrigger, _) => {
-                    println!("Left Trigger pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::LeftTrigger2, _) => {
-                    println!("Left Trigger 2 pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::RightTrigger, _) => {
-                    println!("Right Trigger pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::RightTrigger2, _) => {
-                    println!("Right Trigger 2 pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::Select, _) => {
-                    println!("Select button pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::Start, _) => {
-                    println!("Start button pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::Mode, _) => {
-                    println!("Mode button pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::LeftThumb, _) => {
-                    println!("Left Thumb pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::RightThumb, _) => {
-                    println!("Right Thumb pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::DPadUp, _) => {
-                    println!("D-Pad Up pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::DPadDown, _) => {
-                    println!("D-Pad Down pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::DPadLeft, _) => {
-                    println!("D-Pad Left pressed");
-                }
-                gilrs::EventType::ButtonPressed(Button::DPadRight, _) => {
-                    println!("D-Pad Right pressed");
-                }
-                _ => {}
             }
+            thread::sleep(std::time::Duration::from_millis(100));
+        }
+    });
+
+    let mut keybind_manager = KeybindManager::new();
+
+    // Bind actions to specific keys
+    keybind_manager.bind_key(Keycode::W, {
+        let v_x = v_x.clone();
+        let v_y = v_y.clone();
+        move || {
+            *v_y.lock().unwrap() += 1;
+            println!(
+                "v_x: {}, v_y: {}",
+                *v_x.lock().unwrap(),
+                *v_y.lock().unwrap()
+            );
+        }
+    });
+    keybind_manager.bind_key(Keycode::A, {
+        let v_x = v_x.clone();
+        let v_y = v_y.clone();
+        move || {
+            *v_x.lock().unwrap() -= 1;
+            println!(
+                "v_x: {}, v_y: {}",
+                *v_x.lock().unwrap(),
+                *v_y.lock().unwrap()
+            );
+        }
+    });
+    keybind_manager.bind_key(Keycode::D, {
+        let v_x = v_x.clone();
+        let v_y = v_y.clone();
+        move || {
+            *v_x.lock().unwrap() += 1;
+            println!(
+                "v_x: {}, v_y: {}",
+                *v_x.lock().unwrap(),
+                *v_y.lock().unwrap()
+            );
+        }
+    });
+    keybind_manager.bind_key(Keycode::S, {
+        let v_x = v_x.clone();
+        let v_y = v_y.clone();
+        move || {
+            *v_y.lock().unwrap() -= 1;
+            println!(
+                "v_x: {}, v_y: {}",
+                *v_x.lock().unwrap(),
+                *v_y.lock().unwrap()
+            );
+        }
+    });
+    keybind_manager.bind_key(Keycode::B, {
+        let v_x = v_x.clone();
+        let v_y = v_y.clone();
+        move || {
+            *v_y.lock().unwrap() = 0;
+            *v_x.lock().unwrap() = 0;
+            println!(
+                "v_x: {}, v_y: {}",
+                *v_x.lock().unwrap(),
+                *v_y.lock().unwrap()
+            );
+        }
+    });
+
+    // Main event loop
+    loop {
+        if let Ok(key) = rx.recv() {
+            keybind_manager.handle_key_event(key);
         }
     }
 }
